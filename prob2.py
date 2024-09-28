a608d42c6ee37fdb429d7b99074330f57091bb13
GuildZ <apiwat.cha@kkumail.com>
2024-09-28 19:43:27 +0700

    commit

---
+++ prob2.py
@@ -0,0 +1,43 @@
+
+# Stduent ID1 653040465-3 ดังนั้น y1 = 653
+# Student ID2 653040629-9 ดังนั้น y2 = 299
+# x0 = 653 + 299 
+
+z = ((112*(653+299))%111)+2 # x2
+
+# 1. รหัสและพารามิเตอร์ที่เกี่ยวข้องด้วยโปรแกรมเซจ
+
+# 1.1 พารามิเตอร์ที่เกี่ยวข้อง
+# T h i s i s a s o u r c e c o d e i n S a g e m a t h
+n = 10000 # s a m p l e s i z e
+p = 0.3
+N = 10 # n u m b e r o f t r i a l i n
+
+
+# 1.2 รหัสสำหรับการจำลองลำดับของตัวแปรุส่มและฮิสโทแกรม
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = np.random.binomial(N, p, n)  # Random sequence of Binomial RV.
+minX = X.min()
+maxX = X.max()
+e = np.arange(minX - 0.5, maxX + 1)
+np.histogram(X, bins=e)
+plt.hist(X, bins=e)
+plt.title("Histogram with bins = e")
+plt.show()
+
+Mn = np.mean(X)
+pn = Mn / N
+
+# 1.3 รหัสสำหรับ candidate PMF
+# PMF of Binomial Distributions
+from scipy import stats
+from scipy.stats import binom
+
+k = np.arange(0, N + 1)
+PMF = binom.pmf(k, N, pn)
+fig, ax = plt.subplots(1, 1)
+ax.plot(k, PMF, 'ro', ms=12, mec='r')
+ax.vlines(k, 0, PMF, colors='r', lw=4)
+plt.show()
